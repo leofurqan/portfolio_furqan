4edf901153c1e0b85bcc7504561ebb888b427d1a
Hafiz Furqan Irfan <43038504+leofurqan@users.noreply.github.com>
2024-09-16 15:13:20 +0500

    image upload

--- blogs/models.py
+++ blogs/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Blogs(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    image = models.ImageField(upload_to="blogs/static/blogs/uploads")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(default=timezone.now)
 
